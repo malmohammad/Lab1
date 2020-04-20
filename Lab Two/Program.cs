21267cd2b0273770e6f7a4a01684fc7ee3ab1864
malmohammad <mak_almo@yahoo.com>
2020-04-20 18:17:16 -0400

    added lab

---
+++ Lab Two/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "";
+            do
+            {
+                Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!");
+                Console.WriteLine("Enter Length: ");
+                string lengthString = Console.ReadLine();
+
+                Console.WriteLine("Enter Width: ");
+                string widthString = Console.ReadLine();
+
+                float lengthFloat = float.Parse(lengthString);
+                float widthFloat = float.Parse(widthString);
+
+                float area = lengthFloat + widthFloat;
+                float perimeter = (lengthFloat + widthFloat) * 2;
+
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("perimeter: " + perimeter);
+
+                Console.WriteLine("Do you want to continue (y/n)?");
+                Console.ReadLine();
+            } while (answer == "y");
+        }
+    }
+}
